4796d4a8c2704bfd4bb6181385bac41ee5e2e8be
benzrf <benzrf@benzrf.com>
2013-09-21 16:51:19 -0400

    Added apt-get update to the install script.

--- get_dependencies_ubuntu.sh
+++ get_dependencies_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-add-repository ppa:jon-severinsson/ffmpeg
+sudo apt-get update
 sudo apt-get install cpanminus python-pip imagemagick ffmpeg
 sudo cpanm LWP::Simple JSON
 sudo pip-2.7 install gdata progressbar
