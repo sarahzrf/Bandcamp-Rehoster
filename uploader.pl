079dcd3a0ef4124f1bb1a95b72d03b1ee090073c
benzrf <benzrf@benzrf.com>
2013-09-22 00:45:09 -0400

    now gets properly regex'd in uploader.pl.

--- uploader.pl
+++ uploader.pl
@@ -6,7 +6,7 @@ chdir "mp4s";
 for (<*.mp4>)
 {
 	$title = s/.mp4$//r;
-	$title = s#__SLASH__#/#;
+	$title =~ s#__SLASH__#/#;
 	system("../youtube_upload.py --email='$user' --password='$pass' " .
 		"--title='$title' --description='$title' --category='Music' " . 
 		"--keywords='$title' '$_'") == 0 or exit 1;
